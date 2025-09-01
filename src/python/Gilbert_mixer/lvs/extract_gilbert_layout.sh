73df645048809339b2ef140f33f7bd7c4dd7c146
Vasil Yordanov <vasilradkovyo@gmail.com>
2025-09-01 13:43:24 +0300

    RESTRUCTURE: lvs folder and adjusted scripts appropriately

--- src/python/Gilbert_mixer/lvs/extract_gilbert_layout.sh
+++ src/python/Gilbert_mixer/lvs/extract_gilbert_layout.sh
@@ -5,7 +5,7 @@
 magic -rcfile $PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc -dnull -noconsole << 'EOF'
 gds readonly false
 gds rescale true
-gds read Gilbert_cell_hierarchical.gds
+gds read gds/Gilbert_cell_hierarchical.gds
 
 # Check what cells are available and load the flattened one
 cellname list allcells
@@ -13,16 +13,18 @@ load Gilbert_cell
 
 # Extract with more detailed options
 extract all
+
 ext2spice lvs
-ext2spice cthresh 5
-ext2spice rthresh 1000
+ext2spice cthresh inf
+ext2spice rthresh inf
 ext2spice format ngspice
-ext2spice subcircuits on
+ext2spice subcircuit top auto
 ext2spice hierarchy on
 ext2spice scale off
-ext2spice blackbox off
+ext2spice blackbox on
 ext2spice merge conservative
-ext2spice
+ext2spice global off
+ext2spice -o spice/Gilbert_mixer_extracted_layout.spice
 quit
 EOF
 rm *.ext
--- src/python/Gilbert_mixer/lvs/netgen_compare.sh
+++ src/python/Gilbert_mixer/lvs/netgen_compare.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-netgen -batch lvs "Gilbert_cell.spice Gilbert_cell" "Gilbert_cell_hierarchal_mixing_stage.spice Gilbert_cell_hierarchal_mixing_stage" /usr/local/share/pdk/gf180mcuD/libs.tech/netgen/gf180mcuD_setup.tcl netgen_report
+netgen -batch lvs "spice/Gilbert_mixer_extracted_layout.spice Gilbert_cell" "spice/Gilbert_mixer_extracted_xschem.spice Gilbert_cell_hierarchal_mixing_stage" /usr/local/share/pdk/gf180mcuD/libs.tech/netgen/gf180mcuD_setup.tcl
 
