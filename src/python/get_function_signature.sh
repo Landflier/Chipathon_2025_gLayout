bef2a10006fbe485a5ab3d59bc2b88ee8a2f5f83
Vasil Yordanov <vasilradkovyo@gmail.com>
2025-09-05 16:05:37 +0300

    CHECKPOINT: succesfully placed sd vias. have two variants of interdigitation

--- src/python/get_function_signature.sh
+++ src/python/get_function_signature.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-python -c "import inspect; from glayout.routing.L_route import L_route; print(inspect.signature(L_route))"
+# python -c "import inspect; from glayout.routing.L_route import L_route; print(inspect.signature(L_route))"
+python -c "import inspect; from glayout.util.comp_utils import align_comp_to_port ; print(inspect.signature(align_comp_to_port))"
+
