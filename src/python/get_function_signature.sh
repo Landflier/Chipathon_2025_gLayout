417d13fe02c637f906a8bad5b26005776917e604
Vasil Yordanov <vasilradkovyo@gmail.com>
2025-09-15 16:06:50 +0300

    REORGANIZE: gilbert_mixer_interdigited switched some files arounbd

--- src/python/get_function_signature.sh
+++ src/python/get_function_signature.sh
@@ -4,5 +4,8 @@
 # python -c "import inspect; from glayout.util.comp_utils import align_comp_to_port ; print(inspect.signature(align_comp_to_port))"
 # python -c "import inspect; from glayout.util.comp_utils import prec_array ; print(inspect.signature(prec_array))"
 # python -c "import inspect; from glayout import nmos; print(inspect.signature(nmos))"
-python -c "import inspect; from gdsfactory import Component; print(inspect.signature(Component.write_gds))"
+# python -c "import inspect; from gdsfactory import Component; print(inspect.signature(Component.write_gds))"
+# python -c "import inspect; from gdsfactory import Component; print(inspect.signature(Component.add_padding))"
+# python -c "import inspect; from glayout import tapring; print(inspect.signature(tapring))"
+python -c "import inspect; from glayout import nmos; print(inspect.signature(nmos))"
 
