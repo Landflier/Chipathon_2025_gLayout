f7d60fa5e4dfc5744b08ce7cfc5c78405c396777
Vasil Yordanov <vasilradkovyo@gmail.com>
2025-09-20 18:34:34 +0300

    EDIT: added PEX extraction to the Gilbert_intedigited
    NB: keep names/layout extraction same, since the PEX testbench uses
    these files as includes

--- src/python/Gilbert_mixer_intedigited/lvs/extract_gilbert_layout.sh
+++ src/python/Gilbert_mixer_intedigited/lvs/extract_gilbert_layout.sh
@@ -2,6 +2,7 @@
 
 # ext2spice cthresh 0.01
 
+# LVS
 magic -rcfile $PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc -dnull -noconsole << 'EOF'
 gds readonly false
 gds rescale true
@@ -24,7 +25,36 @@ ext2spice scale off
 ext2spice blackbox on
 ext2spice merge conservative
 ext2spice global off
-ext2spice -o spice/Gilbert_mixer_extracted_layout.spice
+ext2spice -o netlists/Gilbert_mixer_extracted_layout.spice
+quit
+EOF
+
+# PEX
+
+
+magic -rcfile $PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc -dnull -noconsole << 'EOF'
+gds readonly false
+gds rescale true
+gds read gds/Gilbert_cell_interdigited.gds
+
+# Check what cells are available and load the flattened one
+cellname list allcells
+load Gilbert_mixer_interdigited
+cellname rename Gilbert_mixer_interdigited Gilbert_cell_hierarchal_mixing_stage
+
+# Extract with more detailed options
+extract all
+ext2spice lvs
+ext2spice cthresh 1 
+ext2spice rthresh 10 
+ext2spice format ngspice
+ext2spice subcircuit top auto
+ext2spice hierarchy on
+ext2spice scale off
+ext2spice blackbox on
+ext2spice merge conservative
+ext2spice global off
+ext2spice -o netlists/Gilbert_mixer_extracted_layout_PEX.spice
 quit
 EOF
 rm *.ext
