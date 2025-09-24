54d7cf9c83d49499fd67a4866c89ebecb5a18d0a
Vasil Yordanov <vasilradkovyo@gmail.com>
2025-09-24 15:23:43 +0300

    CRITICAL: extract gds from one directory up (not in lvs/gds)

--- src/design_padring/A5_Time_Transcenders_padring_integrated/lvs/extract_gilbert_layout.sh
+++ src/design_padring/A5_Time_Transcenders_padring_integrated/lvs/extract_gilbert_layout.sh
@@ -6,7 +6,7 @@
 magic -rcfile $PDK_ROOT/$PDK/libs.tech/magic/$PDK.magicrc -dnull -noconsole << 'EOF'
 gds readonly false
 gds rescale true
-gds read gds/A5_Gilbert_mixer_only_routing.gds
+gds read ../A5_Gilbert_mixer_only_routing.gds
 
 # Check what cells are available and load the flattened one
 cellname list allcells
